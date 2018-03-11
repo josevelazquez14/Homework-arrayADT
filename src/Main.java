0b82817d0097a6bcd0d0da426b0a726a27c4d6d0
José A. Velázquez Torres <jose.velazquez14@upr.edu>
2018-03-11 12:34:26 -0400

    complete code

--- src/Main.java
+++ src/Main.java
@@ -29,11 +29,11 @@ public class Main {
 
 	
 private static void requestHandler(int request, Object value) {
-//		
-//		if(request == 3) { //prints list size 
-//			int s = array.size();
-//			System.out.print("Size->\""+s+"\"");
-//		}
+		
+		if(request == 3) { //prints list size 
+			int s = array.size();
+			System.out.print("Size->\""+s+"\"");
+		}
 	
 		//will accept entries into arrayList as long as they are not null values
 		if(!value.equals("") && value != null) {
